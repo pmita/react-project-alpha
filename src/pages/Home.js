b932f4d34e4a0c8706bc57655b559404d316513c
panos.mita2@outlook.com <pmita@dxc.com>
2021-06-28 15:46:33 +0100

    projects section added with swiper.js carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from '../components/ServiceSection';
+import ProjectSection from '../components/ProjectSection';
 
 function Home() {
     return (
@@ -10,6 +11,7 @@ function Home() {
             <HeroSection />
             <AboutSection />
             <ServiceSection />
+            <ProjectSection />
         </div>
     )
 }
